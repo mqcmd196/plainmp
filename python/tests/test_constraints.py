bce16a1f4589d0fa34ef88a4c74a1084e1b987c9
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-01 07:09:51 +0900

    Test many case of link pose constraint

--- python/tests/test_constraints.py
+++ python/tests/test_constraints.py
@@ -1,6 +1,7 @@
 import copy
 
 import numpy as np
+import pytest
 from fused.psdf import BoxSDF, Pose
 from fused.robot_spec import FetchSpec
 
@@ -31,16 +32,23 @@ def check_jacobian(const, dim: int, eps: float = 1e-7, decimal: int = 4, std: fl
         np.testing.assert_almost_equal(jac_anal, jac_numel, decimal=decimal)
 
 
-def test_link_pose_constraint():
+@pytest.mark.parametrize("with_rpy", [False, True])
+def test_link_pose_constraint(with_rpy: bool):
+    if with_rpy:
+        pose = [0.7, 0.0, 0.7, 0.0, 0.0, 0.0]
+    else:
+        pose = [0.7, 0.0, 0.7]
+
     fs = FetchSpec()
-    # only gripper
-    cst = fs.create_gripper_pose_const(np.array([0.7, 0.0, 0.7]))
+    cst = fs.create_gripper_pose_const(pose)
     check_jacobian(cst, 8)
 
-    # consider multiple links
-    cst = fs.create_pose_const(
-        ["gripper_link", "wrist_roll_link"], [np.array([0.7, 0.0, 0.7]), np.array([0.7, 0.0, 0.7])]
-    )
+
+def test_link_pose_constraint_multi_link():
+    pose1 = [0.7, 0.0, 0.7, 0.0, 0.0, 0.0]
+    pose2 = [0.7, 0.0, 0.7]
+    fs = FetchSpec()
+    cst = fs.create_pose_const(["gripper_link", "wrist_roll_link"], [pose1, pose2])
     check_jacobian(cst, 8)
 
 
@@ -51,7 +59,3 @@ def test_collision_free_constraint():
         cst = fs.create_collision_const(self_collision)
         cst.set_sdfs([sdf])
         check_jacobian(cst, 8)
-
-
-if __name__ == "__main__":
-    test_collision_free_constraint()
