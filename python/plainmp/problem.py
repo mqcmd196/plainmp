05054418455538f39ed8a1c82682e9114e9b808c
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-03 21:43:16 +0900

    Add check_init_feasibility

--- python/plainmp/problem.py
+++ python/plainmp/problem.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Optional, Union, Tuple
 
 import numpy as np
 
@@ -15,3 +15,11 @@ class Problem:
     global_ineq_const: Optional[IneqConstraintBase]
     global_eq_const: Optional[EqConstraintBase]
     motion_step_box: np.ndarray
+
+    def check_init_feasibility(self) -> Tuple[bool, str]:
+        if not (np.all(self.lb <= self.start) and np.all(self.start <= self.ub)):
+            return False, "Start point is out of bounds"
+        if self.global_ineq_const is not None:
+            if not self.global_ineq_const.is_valid(self.start):
+                return False, "Start point violates global inequality constraints"
+        return True, ""
