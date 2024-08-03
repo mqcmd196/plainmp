46a9bb5e047504060227b22c9cf0195cd8b17b6d
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-04 07:07:49 +0900

    Make pip installable

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 try:
-    # from skbuild import setup
-    from setuptools import setup
+    from skbuild import setup
 except ImportError:
     raise Exception
 
 setup(
     name="plainmp",
-    version="0.0.1",
+    version="0.0.2",
     description="experimental",
     author="Hirokazu Ishida",
     license="MIT",
-    install_requires=["numpy"],
+    install_requires=["numpy", "scipy", "scikit-robot"],
     packages=["plainmp"],
     package_dir={"": "python"},
-    package_data={"plainmp": ["__init__.pyi"]},
+    package_data={"plainmp": ["*.pyi", "conf/*.yaml"]},
     cmake_install_dir="python/plainmp/",
 )
