a5ad778de3cada6798edc4151ceb134d491ab689
HiroIshida <h-ishida@jsk.imi.i.u-tokyo.ac.jp>
2024-08-10 22:59:23 +0900

    feat: add set_robot_state

--- python/plainmp/utils.py
+++ python/plainmp/utils.py
@@ -1,3 +1,9 @@
+from typing import List
+
+import numpy as np
+from skrobot.coordinates import Coordinates
+from skrobot.coordinates.math import rpy_matrix
+from skrobot.model.robot_model import RobotModel
 from skrobot.sdf import BoxSDF, CylinderSDF, UnionSDF
 
 import plainmp.psdf as psdf
@@ -19,3 +25,23 @@ def sksdf_to_cppsdf(sksdf, create_bvh: bool = False) -> psdf.SDFBase:
     else:
         raise ValueError(f"Unsupported SDF type {type(sksdf)}")
     return sdf
+
+
+def set_robot_state(
+    robot_model: RobotModel,
+    joint_names: List[str],
+    angles: np.ndarray,
+    floating_base: bool = False,
+) -> None:
+    if floating_base:
+        assert len(joint_names) + 6 == len(angles)
+        av_joint, av_base = angles[:-6], angles[-6:]
+        xyz, rpy = av_base[:3], av_base[3:]
+        co = Coordinates(pos=xyz, rot=rpy_matrix(*np.flip(rpy)))
+        robot_model.newcoords(co)
+    else:
+        assert len(joint_names) == len(angles)
+        av_joint = angles
+
+    for joint_name, angle in zip(joint_names, av_joint):
+        robot_model.__dict__[joint_name].joint_angle(angle)
